5b3431ed52debcd50fd2eb9b80f8d4e5cf5ce8ef
Lindsay Wardell <three060@gmail.com>
2020-09-06 14:07:51 -0700

    chore: added test cases for error states

--- test/module.test.js
+++ test/module.test.js
@@ -4,7 +4,7 @@ describe('module', () => {
   let nuxt
 
   beforeAll(async () => {
-    ({ nuxt } = (await setup(loadConfig(__dirname, '../../example'))))
+    ;({ nuxt } = await setup(loadConfig(__dirname, '../../example')))
   }, 60000)
 
   afterAll(async () => {
@@ -12,7 +12,38 @@ describe('module', () => {
   })
 
   test('render', async () => {
+    // ;({ nuxt } = await setup(loadConfig(__dirname, '../../example')))
+
     const html = await get('/')
     expect(html).toContain('Lindsay Wardell')
+  })
+
+  test('throws error when no Github token is present', async () => {
+    try {
+      ;({ nuxt } = await setup(
+        loadConfig(__dirname, '../../example', {
+          githubApi: {
+            token: null
+          }
+        })
+      ))
+    } catch (err) {
+      expect(err.toString().includes('Missing GitHub API token!')).toBe(true)
+    }
+  })
+
+  test('throws error when query is invalid', async () => {
+    try {
+      ;({ nuxt } = await setup(
+        loadConfig(__dirname, '../../example', {
+          githubApi: {
+            token: process.env.GITHUB_TOKEN,
+            graphQLQuery: 'invalid query'
+          }
+        })
+      ))
+    } catch (err) {
+      expect(err.toString().includes('GitHub API queries must be wrapped in `query{}`')).toBe(true)
+    }
   })
 })
